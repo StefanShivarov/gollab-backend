497d6cda9c9fafd8e14e2319dd8b017b247d79e8
StefanShivarov <stefan.shivarov.jr@gmail.com>
2026-01-01 19:59:02 +0200

    Refactor routing logic

--- internal/org/routes.go
+++ internal/org/routes.go
@@ -2,7 +2,7 @@ package org
 
 import "github.com/go-chi/chi/v5"
 
-func Routes(r chi.Router, handler *UserHandler) {
+func UserRoutes(r chi.Router, handler *UserHandler) {
 	r.Route("/users", func(r chi.Router) {
 		r.Get("/", handler.List)
 		r.Post("/", handler.Create)
@@ -11,6 +11,25 @@ func Routes(r chi.Router, handler *UserHandler) {
 			r.Get("/", handler.GetByID)
 			r.Put("/", handler.UpdateByID)
 			r.Delete("/", handler.DeleteByID)
+		})
+	})
+}
+
+func TeamRoutes(r chi.Router, handler *TeamHandler) {
+	r.Route("/teams", func(r chi.Router) {
+		r.Get("/", handler.List)
+		r.Post("/", handler.Create)
+
+		r.Route("/{teamId}", func(r chi.Router) {
+			r.Get("/", handler.GetByID)
+			r.Put("/", handler.UpdateByID)
+			r.Delete("/", handler.DeleteByID)
+
+			r.Route("/members", func(r chi.Router) {
+				r.Get("/", handler.ListTeamMembers)
+				r.Post("/", handler.AddMember)
+				r.Delete("/", handler.RemoveMember)
+			})
 		})
 	})
 }
