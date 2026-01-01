74376e495f8e5cfe11e939393159ddf14f438f12
StefanShivarov <stefan.shivarov.jr@gmail.com>
2026-01-01 19:56:26 +0200

    Add more dtos

--- internal/org/dto.go
+++ internal/org/dto.go
@@ -25,3 +25,45 @@ func ToUserResponse(user *User) *UserResponse {
 		Name:  user.Name,
 	}
 }
+
+type CreateTeamRequest struct {
+	Name        string `json:"name" validate:"required,min=2,max=50"`
+	Description string `json:"description"`
+}
+
+type UpdateTeamRequest struct {
+	Name        string `json:"name" validate:"omitempty,min=2,max=50"`
+	Description string `json:"description"`
+}
+
+type TeamResponse struct {
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+}
+
+func ToTeamResponse(team *Team) *TeamResponse {
+	return &TeamResponse{
+		ID:          team.ID,
+		Name:        team.Name,
+		Description: team.Description,
+	}
+}
+
+type CreateMembershipRequest struct {
+	TeamID uuid.UUID `json:"teamId" validate:"required,uuid"`
+	UserID uuid.UUID `json:"userId" validate:"required,uuid"`
+	Role   TeamRole  `json:"role" validate:"required,oneof=project_manager developer"`
+}
+
+type DeleteMembershipRequest struct {
+	TeamID uuid.UUID `json:"teamId" validate:"required,uuid"`
+	UserID uuid.UUID `json:"userId" validate:"required,uuid"`
+}
+
+type MemberResponse struct {
+	ID    uuid.UUID `json:"id"`
+	Name  string    `json:"username"`
+	Email string    `json:"email"`
+	Role  TeamRole  `json:"role"`
+}
